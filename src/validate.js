ceda647241fe8842d75a2ca3db4c3bdd2dede8cd
Frank Rowe <fsrowe@gmail.com>
2016-09-27 11:11:08 -0400

    remove arr prototype

--- src/validate.js
+++ src/validate.js
@@ -1,13 +1,6 @@
 import { Validator } from 'jsonschema';
 import _ from 'lodash';
 
-// Array.prototype.unique = function() {
-//   var o = {}, i, l = this.length, r = [];
-//   for(i=0; i<l;i+=1) o[this[i]] = this[i];
-//   for(i in o) r.push(o[i]);
-//   return r;
-// };
-
 let formSchema = {
   "id": "/SCForm",
   "type": "object",
