9a35f4782eb36076943635f28bbf02edaf479c0c
Josh Kendrick <joshkendrick@users.noreply.github.com>
2017-07-11 11:04:28 -0500

    fix and finish validation checks

--- src/validateFields.js
+++ src/validateFields.js
@@ -21,17 +21,20 @@ function validateFormField(field) {
     return true;
   }
 
-  let value = field.props.value;
+  let value = field.getValue();
   let config = options.hasOwnProperty('config') ? options.config : {};
   let errorMsgs = [];
 
-  if (config.hasOwnProperty('required')) {
-    let required = config.required;
-    if (required && !value) {
+  // required - value must be provided
+  if (config.hasOwnProperty('required') && config.required && !value) {
+    // allow booleans to be false (yes/no fields)
+    // allow number fields (esp. sliders) to be 0
+    if ((config.type == 'number' && value != 0) || config.type != 'boolean') {
       errorMsgs.push('value is required');
     }
   }
 
+  // minimum - value must be numerically >= to minimum
   if (config.hasOwnProperty('minimum')) {
     let minimum = Number(config.minimum);
     if (value < minimum) {
@@ -39,10 +42,35 @@ function validateFormField(field) {
     }
   }
 
+  // maximum - value must be numerically <= to maximum
   if (config.hasOwnProperty('maximum')) {
     let maximum = Number(config.maximum);
     if (value > maximum) {
       errorMsgs.push('value must be <= ' + maximum);
+    }
+  }
+
+  // minLength - value must have at least minLength characters
+  if (config.hasOwnProperty('minLength')) {
+    let minLength = config.minLength;
+    if (String(value).length < minLength) {
+      errorMsgs.push('value must have ' + minLength + ' characters');
+    }
+  }
+
+  // maxLength - value must have less than maxLength characters
+  if (config.hasOwnProperty('maxLength')) {
+    let maxLength = config.maxLength;
+    if (String(value).length > maxLength) {
+      errorMsgs.push('value must have less than ' + maxLength + ' characters');
+    }
+  }
+
+  // pattern - value must match specified regular expression
+  if (config.hasOwnProperty('pattern')) {
+    let pattern = RegExp(config.pattern);
+    if (!pattern.test(value)) {
+      errorMsgs.push('value does not match pattern: ' + pattern);
     }
   }
 
